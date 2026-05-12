9af1525deace8e4f7440f3f8253f0886731b57fd
Luan <l.hyseni@tu-braunschweig.de>
2026-05-12 10:06:01 +0200

    pass risks data to template

--- app.py
+++ app.py
@@ -2,13 +2,44 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+risks_data = [
+    {
+        "name": "Brute Force Attack",
+        "description": "Multiple failed SSH login attempts detected.",
+        "category": "Authentication",
+        "score": 20,
+        "status": "Open"
+    },
+
+    {
+        "name": "Weak Password Policy",
+        "description": "Passwords do not meet complexity requirements.",
+        "category": "Authentication",
+        "score": 15,
+        "status": "In Progress"
+    },
+
+    {
+        "name": "Missing MFA",
+        "description": "Multi-factor authentication is not enabled.",
+        "category": "Identity Security",
+        "score": 12,
+        "status": "Open"
+    }
+]
+
+
+
 @app.route("/")
 def dashboard():
     return render_template("dashboard.html")
 
 @app.route("/risks")
 def risks():
-    return render_template("risks.html")
+    print(risks_data)
+    return render_template("risks.html", risks=risks_data)
+
+
 
 
 if __name__ == "__main__":
